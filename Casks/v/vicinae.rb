e52c0bd2f5a0b86eee1dd6a19c6e1a217d7dabcf
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-08-27 13:21:31 +0600

    vicinae: add AppImage

--- Casks/v/vicinae.rb
+++ Casks/v/vicinae.rb
@@ -1,26 +1,45 @@
 cask "vicinae" do
-  version "0.27.1"
-  sha256 "20d685d4f03845f8a555a14bc898dcd4d7e7d5f9ff3c9cf69e6cee945c653c65"
+  arch arm: "aarch64", intel: "x86_64"
+  os macos: ".dmg", linux: "-#{arch}.AppImage"
 
-  url "https://github.com/vicinaehq/vicinae/releases/download/v#{version}/Vicinae.dmg",
+  version "0.27.1"
+
+  on_macos do
+    sha256 "20d685d4f03845f8a555a14bc898dcd4d7e7d5f9ff3c9cf69e6cee945c653c65"
+
+    depends_on arch: :arm64
+    depends_on macos: :sonoma
+
+    app "Vicinae.app"
+    binary "#{appdir}/Vicinae.app/Contents/MacOS/vicinae-cli", target: "vicinae"
+
+    zap trash: [
+      "~/.cache/vicinae",
+      "~/.config/vicinae",
+      "~/.local/share/vicinae",
+      "~/.local/state/vicinae",
+      "~/Library/Caches/com.vicinaehq.Vicinae",
+      "~/Library/Caches/vicinae",
+      "~/Library/HTTPStorages/com.vicinaehq.Vicinae",
+    ]
+  end
+  on_linux do
+    sha256 arm64_linux:  "a4b85d0a2d24a949c4c23b22fc6e47ca98c3b5af51ed96b540942971a40f9b34",
+           x86_64_linux: "060a3bf2dde7483404a39faee0a5695fc721aa1ac9c18eb5bcbee8a8406c8e6b"
+
+    app_image "Vicinae-#{arch}.AppImage", target: "Vicinae.AppImage"
+
+    zap trash: [
+      "~/.cache/vicinae",
+      "~/.config/vicinae",
+      "~/.local/share/vicinae",
+      "~/.local/state/vicinae",
+    ]
+  end
+
+  url "https://github.com/vicinaehq/vicinae/releases/download/v#{version}/Vicinae#{os}",
       verified: "github.com/vicinaehq/vicinae/"
   name "Vicinae"
   desc "Application launcher and command palette"
   homepage "https://vicinae.com/"
-
-  depends_on arch: :arm64
-  depends_on macos: :sonoma
-
-  app "Vicinae.app"
-  binary "#{appdir}/Vicinae.app/Contents/MacOS/vicinae-cli", target: "vicinae"
-
-  zap trash: [
-    "~/.cache/vicinae",
-    "~/.config/vicinae",
-    "~/.local/share/vicinae",
-    "~/.local/state/vicinae",
-    "~/Library/Caches/com.vicinaehq.Vicinae",
-    "~/Library/Caches/vicinae",
-    "~/Library/HTTPStorages/com.vicinaehq.Vicinae",
-  ]
 end
